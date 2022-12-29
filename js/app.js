6610c8359a78371062361b750fc1643dff21b06e
Franco <francoherrera77003@gmail.com>
2022-12-28 23:30:39 -0300

    hago la peticion en app.js

---
+++ js/app.js
@@ -0,0 +1,33 @@
+const cargarPeliculas = async () => {
+
+    try {
+
+        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=7259b0522d8ef15b8214df3c83c5c11e&language=es-MX&page=${pagina}`)
+
+        if (respuesta.status === 200) {
+            
+            const datos = await respuesta.json()
+
+            let peliculas = ''
+
+            datos.results.forEach(pelicula => {
+                peliculas += `
+                    <div class = "pelicula">
+                        <img class = "poster" src = "https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                    </div>
+                    <h3 class= "titulo">${pelicula.title}</h3>
+                `
+            });
+
+            document.getElementById('contenedor').innerHTML = peliculas
+
+        } else {
+            console.log('Hubo un error')
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+cargarPeliculas()
