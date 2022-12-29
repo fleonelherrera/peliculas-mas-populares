66d52bf2adf79f21adcf53faf337126f6a59af46
Franco <francoherrera77003@gmail.com>
2022-12-29 18:38:49 -0300

    agrego botones next y back

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const cargarPeliculas = async () => {
 
     try {
 
-        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=7259b0522d8ef15b8214df3c83c5c11e&language=es-MX`)
+        const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=7259b0522d8ef15b8214df3c83c5c11e`)
 
         if (respuesta.status === 200) {
 
